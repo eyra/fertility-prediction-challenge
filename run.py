d048ffe2f5f5ca8d7a7f152260bbd53b1dadc2e7
Jeroen <jeroen@adaptiveconstant.com>
2024-03-05 09:39:04 +0100

    Applied feedback (#16)

--- run.py
+++ run.py
@@ -12,69 +12,47 @@ should be 0 (no child) or 1 (having a child).
 
 The script can be run from the command line using the following command:
 
-python script.py input_path 
+python run.py input_path 
 
 An example for the provided test is:
 
-python script.py data/test_data_liss_2_subjects.csv
+python run.py data/test_data_liss_2_subjects.csv
 """
 
-import os
 import sys
 import argparse
 import pandas as pd
-from joblib import load
+import submission
 
 parser = argparse.ArgumentParser(description="Process and score data.")
 subparsers = parser.add_subparsers(dest="command")
 
 # Process subcommand
-process_parser = subparsers.add_parser("predict", help="Process input data for prediction.")
+process_parser = subparsers.add_parser(
+    "predict", help="Process input data for prediction."
+)
 process_parser.add_argument("input_path", help="Path to input data CSV file.")
 process_parser.add_argument("--output", help="Path to prediction output CSV file.")
 
 # Score subcommand
 score_parser = subparsers.add_parser("score", help="Score (evaluate) predictions.")
 score_parser.add_argument("prediction_path", help="Path to predicted outcome CSV file.")
-score_parser.add_argument("ground_truth_path", help="Path to ground truth outcome CSV file.")
+score_parser.add_argument(
+    "ground_truth_path", help="Path to ground truth outcome CSV file."
+)
 score_parser.add_argument("--output", help="Path to evaluation score output CSV file.")
 
 args = parser.parse_args()
 
 
-def predict_outcomes(df):
-    """Process the input data and write the predictions."""
-
-    # The predict_outcomes function accepts a Pandas DataFrame as an argument
-    # and returns a new DataFrame with two columns: nomem_encr and
-    # prediction. The nomem_encr column in the new DataFrame replicates the
-    # corresponding column from the input DataFrame. The prediction
-    # column contains predictions for each corresponding nomem_encr. Each
-    # prediction is represented as a binary value: '0' indicates that the
-    # individual did not have a child during 2020-2022, while '1' implies that
-    # they did.
-
-    # Keep 
-    keepcols = ['burgstat2019', 'leeftijd2019', 'woonvorm2019', 'oplmet2019', 'aantalki2019']
-    nomem_encr = df["nomem_encr"]
-    
-    df = df.loc[:, keepcols]
-    
-    # Load your trained model from the models directory
-    model_path = os.path.join(os.path.dirname(__file__), "..", "models", "model.joblib")
-    model = load(model_path)
-
-    # Use your trained model for prediction
-    predictions = model.predict(df)
-    # Return the result as a Pandas DataFrame with the columns "nomem_encr" and "prediction"
-    return pd.concat([nomem_encr, pd.Series(predictions, name="prediction")], axis=1)
-
-
 def predict(input_path, output):
     if output is None:
         output = sys.stdout
-    df = pd.read_csv(input_path, encoding="latin-1", encoding_errors="replace", low_memory=False)
-    predictions = predict_outcomes(df)
+    df = pd.read_csv(
+        input_path, encoding="latin-1", encoding_errors="replace", low_memory=False
+    )
+    df = submission.clean_df(df)
+    predictions = submission.predict_outcomes(df)
     assert (
         predictions.shape[1] == 2
     ), "Predictions must have two columns: nomem_encr and prediction"
@@ -88,10 +66,10 @@ def predict(input_path, output):
 
 def score(prediction_path, ground_truth_path, output):
     """Score (evaluate) the predictions and write the metrics.
-    
+
     This function takes the path to a CSV file containing predicted outcomes and the
-    path to a CSV file containing the ground truth outcomes. It calculates the overall 
-    prediction accuracy, and precision, recall, and F1 score for having a child 
+    path to a CSV file containing the ground truth outcomes. It calculates the overall
+    prediction accuracy, and precision, recall, and F1 score for having a child
     and writes these scores to a new output CSV file.
 
     This function should not be modified.
@@ -107,9 +85,9 @@ def score(prediction_path, ground_truth_path, output):
     merged_df = pd.merge(predictions_df, ground_truth_df, on="nomem_encr", how="right")
 
     # Calculate accuracy
-    accuracy = len(
-        merged_df[merged_df["prediction"] == merged_df["new_child"]]
-    ) / len(merged_df)
+    accuracy = len(merged_df[merged_df["prediction"] == merged_df["new_child"]]) / len(
+        merged_df
+    )
 
     # Calculate true positives, false positives, and false negatives
     true_positives = len(
@@ -136,13 +114,16 @@ def score(prediction_path, ground_truth_path, output):
     except ZeroDivisionError:
         f1_score = 0
     # Write metric output to a new CSV file
-    metrics_df = pd.DataFrame({
-        'accuracy': [accuracy],
-        'precision': [precision],
-        'recall': [recall],
-        'f1_score': [f1_score]
-    })
+    metrics_df = pd.DataFrame(
+        {
+            "accuracy": [accuracy],
+            "precision": [precision],
+            "recall": [recall],
+            "f1_score": [f1_score],
+        }
+    )
     metrics_df.to_csv(output, index=False)
+
 
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -152,5 +133,5 @@ if __name__ == "__main__":
         score(args.prediction_path, args.ground_truth_path, args.output)
     else:
         parser.print_help()
-        predict(args.input_path, args.output)  
+        predict(args.input_path, args.output)
         sys.exit(1)
