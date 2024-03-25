099d0ce8e6a3682b97eac430abb9d07ac53175a9
gertstulp <g.stulp@rug.nl>
2024-03-25 16:54:35 +0100

    added argument for outcome file R

--- training.R
+++ training.R
@@ -6,17 +6,21 @@
 # It is important to document your training steps here, including seed, 
 # number of folds, model, et cetera
 
-train_save_model <- function(clean_df) {
+train_save_model <- function(clean_df, outcome_df) {
   # Trains a model using the cleaned dataframe and saves the model to a file.
 
   # Parameters:
   # cleaned_df (dataframe): The cleaned data from clean_df function to be used for training the model.
+  # outcome_df (dataframe): The data with the outcome variable (e.g., from PreFer_train_outcome.csv or PreFer_fake_outcome.csv).
 
   ## This script contains a bare minimum working example
   set.seed(1) # not useful here because logistic regression deterministic
   
+  # Combine cleaned_df and outcome_df
+  model_df <- merge(clean_df, outcome_df, by = "nomem_encr")
+  
   # Logistic regression model
-  model <- glm(new_child ~ age, family = "binomial", data = clean_df)
+  model <- glm(new_child ~ age, family = "binomial", data = model_df)
   
   # Save the model
   saveRDS(model, "model.rds")
