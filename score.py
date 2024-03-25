6748bb9d76abaa3619f458896a8c9622f68f0d2b
Adrienne Mendrik <79082794+AdrienneMendrik@users.noreply.github.com>
2024-03-25 09:31:05 +0100

    Update score.py

--- score.py
+++ score.py
@@ -3,11 +3,11 @@ This script is used to calculate the metrics on the challenge leaderboards.
 
 It can be used with the following command:
 
-python score.py score 
+python score.py 
 
 For example:
 
-python score.py score PreFer_fake_data_predictions.csv PreFer_fake_data_outcome.csv
+python score.py PreFer_fake_data_predictions.csv PreFer_fake_data_outcome.csv
 
 Note: The ground truth (outcome) needs to be in a seperate file with two columns (nomem_encr, new_child)
 
